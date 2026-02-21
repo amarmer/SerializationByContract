4604ef8d8e92190877cd1baba79f88acd60a4195
Alex Marmer <amarmer@gmail.com>
2026-02-21 13:14:27 -0800

    Added dispatching in IPC

--- SerializationContract.h
+++ SerializationContract.h
@@ -5,22 +5,25 @@
 #include "SerializationContractData.h"
 
 namespace SerializationContract {
-  template <typename T> 
-  struct Processor: public Processor<decltype(&T::operator())>{};
+  //
+  // Processor
+  //
+  template <const char* Name, typename T>
+  struct Processor;
 
-  template <typename ClassType, typename... Params>
-  struct Processor<void(ClassType::*)(Params...) const> {
+  template <const char* Name, typename... Params>
+  struct Processor<Name, std::function<void(Params...)>> {
     template <typename IsConstParams, typename ...Ts>
     auto CreateTupleWithParamsProxy(Ts&&... ts) {
       auto tuple = std::tuple<Ts&...>(std::forward<Ts&>(ts)...);
       return TupleWithParamsProxy<IsConstParams, decltype(tuple)>{std::move(tuple)};
     }
-      
+
     // Used only for serialization.
     auto operator ()(const Params&... params) {
       return CreateTupleWithParamsProxy<std::true_type>(std::forward<const Params&>(params)...);
     }
-      
+
     // Used for serialization and unserialization.
     auto operator ()(Params&... params) {
       return CreateTupleWithParamsProxy<std::false_type>(std::forward<Params&>(params)...);
@@ -28,8 +31,8 @@ namespace SerializationContract {
 
     template <typename IsConstParams, typename TupleWithParams>
     struct TupleWithParamsProxy {
-      static constexpr auto LastIndex = std::tuple_size_v<TupleWithParams> - 1;
-        
+      static constexpr auto LastTupleIndex = std::tuple_size_v<TupleWithParams> -1;
+
       // Serialization        
       void operator >> (std::vector<uint8_t>& bytes) {
         Serializer serializer(bytes);
@@ -38,9 +41,13 @@ namespace SerializationContract {
 
       template<int Index>
       void SerializeParams(Serializer& serializer) {
+        if constexpr (Index == 0) {
+          serializer << std::string(Name);
+        }
+
         serializer << std::get<Index>(tupleWithParams_);
 
-        if constexpr (Index < LastIndex) {
+        if constexpr (Index < LastTupleIndex) {
           SerializeParams<Index + 1>(serializer);
         }
       }
@@ -48,16 +55,21 @@ namespace SerializationContract {
       // Unserialization
       void operator << (const std::vector<uint8_t>& bytes) {
         static_assert(std::is_same_v<IsConstParams, std::false_type>, "Cannot unserialize to const");
-          
+
         Unserializer unserializer(bytes);
         UnserializeParams<0>(unserializer);
       }
 
       template<int Index>
       void UnserializeParams(Unserializer& unserializer) {
+        if constexpr (Index == 0) {
+          std::string name;
+          unserializer >> name;
+        }
+
         unserializer >> std::get<Index>(tupleWithParams_);
 
-        if constexpr (Index < LastIndex) {
+        if constexpr (Index < LastTupleIndex) {
           UnserializeParams<Index + 1>(unserializer);
         }
       }
@@ -65,10 +77,114 @@ namespace SerializationContract {
       TupleWithParams tupleWithParams_;
     };
   };
-}
-    
-#define SERIALIZATION_CONTRACT(x, ...)                                                                       \
-  static auto x = [](auto&&... params) {                                                                     \
-    auto lambda = [](__VA_ARGS__) {};                                                                        \
-    return SerializationContract::Processor<decltype(lambda)>()(std::forward<decltype(params)>(params)...);  \
+
+  //
+  // UnserializeDispatcher
+  //
+  class UnserializeDispatcher {
+  public:
+    static UnserializeDispatcher& Instance() {
+      static UnserializeDispatcher s_unserializeDispatcher;
+      return s_unserializeDispatcher;
+    }
+
+    struct IDispatcher {
+      virtual ~IDispatcher() = default;
+
+      virtual bool Dispatch(const std::string& contractName, Unserializer& unserializer) = 0;
+    };
+
+    template <typename F, const char* Name, typename... Params>
+    class TDispatcher : public IDispatcher {
+    public:
+      TDispatcher(F f)
+        : f_(f)
+      {}
+
+      template <typename ...> struct ArgsCollector;
+
+      template <typename T, typename ...Ts>
+      struct ArgsCollector<T, Ts...> {
+        template <typename ...Args>
+        static void CollectArgs(F f, Unserializer& unserializer, const Args&...args) {
+          T arg;
+          unserializer >> arg;
+
+          if constexpr (0 == sizeof...(Ts)) {
+            f(args..., (const T&)arg);
+          }
+          else {
+            ArgsCollector<Ts...>::template CollectArgs<>(f, unserializer, args..., arg);
+          }
+        }
+      };
+
+      bool Dispatch(const std::string& contractName, Unserializer& unserializer) override {
+        if (contractName != Name) {
+          return false;
+        }
+
+        ArgsCollector<Params...>::template CollectArgs<>(f_, unserializer);
+
+        return true;
+      }
+
+      F f_;
+    };
+
+    bool Dispatch(const std::vector<uint8_t>& bytes) {
+      Unserializer unserializer(bytes);
+
+      std::string contractName;
+      unserializer >> contractName;
+
+      bool dispatched = false;
+
+      for (const auto& pDispatcher : vDispatcher_) {
+        if (pDispatcher->Dispatch(contractName, unserializer)) {
+          dispatched = true;
+          break;
+        }
+      }
+
+      return dispatched;
+    }
+
+    template <const char* Name, typename... Params, typename F>
+    void Subscribe(const Processor<Name, std::function<void(Params...)>>&, F f) {
+      vDispatcher_.emplace_back(std::make_unique<TDispatcher<F, Name, Params...>>(f));
+    }
+
+    std::vector<std::unique_ptr<IDispatcher>> vDispatcher_;
   };
+
+  //
+  // UnserializeDispatcherProxy
+  //
+  template <const char* Name, typename... Params>
+  class UnserializeDispatcherProxy {
+  public:
+    UnserializeDispatcherProxy(const Processor<Name, std::function<void(Params...)>>& processor)
+      : processor_(processor)
+    {}
+
+    template <typename F>
+    bool operator = (F f)
+    {
+      UnserializeDispatcher::Instance().Subscribe(processor_, f);
+      return true;
+    }
+
+    Processor<Name, std::function<void(Params...)>> processor_;
+  };
+}
+
+#define SERIALIZATION_CONTRACT(x, ...)                                                                                      \
+  [[maybe_unused]] inline static constexpr char s_contractName##x[] = #x;                                                   \
+  [[maybe_unused]] static auto x = SerializationContract::Processor<s_contractName##x, std::function<void(__VA_ARGS__)>>(); \
+
+#define ON_SERIALIZATION_CONTRACT(x) \
+    [[maybe_unused]] static bool s_onContract##x = SerializationContract::UnserializeDispatcherProxy(x) = 
+
+#define PROCESS_SERIALIZATION_CONTRACT(bytes) \
+  SerializationContract::UnserializeDispatcher::Instance().Dispatch(bytes);
